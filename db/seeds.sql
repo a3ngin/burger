16b69613e134f7106ef7a2645d631ed44f5ad620
Anne Engin <anne@Annes-MacBook-Air.local>
2019-04-07 18:56:44 -0400

    kind of working

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name) VALUES ('Plain');
+INSERT INTO burgers (name, triedYet) VALUES ('Bacon', true);
+INSERT INTO burgers (name, triedYet) VALUES ('Mushroom', true);
+INSERT INTO burgers (name, TriedYet) VALUES ('Crispy', true);
+INSERT INTO burgers (name) VALUES ('Turkey');
